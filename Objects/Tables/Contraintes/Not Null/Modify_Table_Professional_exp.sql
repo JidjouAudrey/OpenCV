986b066367d19e5d5a3babf132ee643115ea44c3
BieleuRoussel <bieleuroussel@gmail.com>
2022-11-15 22:17:58 +0200

    Ajout des Datas et des contraintes

---
+++ Objects/Tables/Contraintes/Not Null/Modify_Table_Professional_exp.sql
@@ -0,0 +1,9 @@
+ALTER TABLE Professional_exp(  MODIFY id_expro INT NOT NULL,
+                         MODIFY status_expro VARCHAR(30) NOT NULL,
+                         MODIFY organisation INT NOT NULL,
+                         MODIFY function_exp VARCHAR(30) NOT NULL,
+                         MODIFY startDate DATE NOT NULL,
+                         MODIFY endDate DATE NOT NULL,
+                         MODIFY id_exp INT NOT NULL
+);
+
