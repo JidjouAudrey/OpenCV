09ae8dc14b1bf2e3a87a9be8b63faf196ef0d298
unknown <peughouia.44@gmail.com>
2022-11-15 20:14:01 +0100

    donnee

---
+++ Data/D_profil.sql
@@ -0,0 +1,44 @@
+INSERT INTO Profil (id_profil,firstname,lastname,profilImage,borndate,bornAt,maritalStatus,numberChild,drivingLicence,id_user,id_city,id_country)
+VALUE(1,'Peughouia','fred','png','12/04/2001','douala','Celibataire','0','permis b',1,1,1);
+
+INSERT INTO Profil (id_profil,firstname,lastname,profilImage,borndate,bornAt,maritalStatus,numberChild,drivingLicence,id_user,id_city,id_country)
+VALUE(2,'Tanko','leila','jpg','07/04/2003','douala','Couple','0','permis D',2,2,2);
+
+INSERT INTO Profil (id_profil,firstname,lastname,profilImage,borndate,bornAt,maritalStatus,numberChild,drivingLicence,id_user,id_city,id_country)
+VALUE(3,'Tekoudjou','loic','png','12/05/2000','bafoussam','Celibataire','0','permis D',3,3,3);
+
+INSERT INTO Profil (id_profil,firstname,lastname,profilImage,borndate,bornAt,maritalStatus,numberChild,drivingLicence,id_user,id_city,id_country)
+VALUE(4,'Dongmo','anny','png','11/10/1985','yaounde','Celibataire','3','permis B',4,4,4);
+
+INSERT INTO Profil (id_profil,firstname,lastname,profilImage,borndate,bornAt,maritalStatus,numberChild,drivingLicence,id_user,id_city,id_country)
+VALUE(5,'Kombo','andrea','png','11/03/2001','bafoussam','couple','7','permis D',5,5,5);
+
+INSERT INTO Profil (id_profil,firstname,lastname,profilImage,borndate,bornAt,maritalStatus,numberChild,drivingLicence,id_user,id_city,id_country)
+VALUE(6,'Donfack','marie','png','10/02/1985','yaounde','Celibataire','1','permis C',6,6,6);
+
+INSERT INTO Profil (id_profil,firstname,lastname,profilImage,borndate,bornAt,maritalStatus,numberChild,drivingLicence,id_user,id_city,id_country)
+VALUE(7,'Lontsi','olive','png','16/10/1985','bamenda','Celibataire','8','permis E',7,7,7);
+
+INSERT INTO Profil (id_profil,firstname,lastname,profilImage,borndate,bornAt,maritalStatus,numberChild,drivingLicence,id_user,id_city,id_country)
+VALUE(8,'Dongmo','loic','png','11/12/1988','yaounde','Celibataire','5','permis E',8,8,8);
+
+INSERT INTO Profil (id_profil,firstname,lastname,profilImage,borndate,bornAt,maritalStatus,numberChild,drivingLicence,id_user,id_city,id_country)
+VALUE(9,'Tedongmo','armel','png','22/06/1999','Garoua','Celibataire','4','permis D',9,9,9);
+
+INSERT INTO Profil (id_profil,firstname,lastname,profilImage,borndate,bornAt,maritalStatus,numberChild,drivingLicence,id_user,id_city,id_country)
+VALUE(10,'Momdjo','uriel','png','22/09/1995','maroua','Celibataire','5','permis A',10,10,10);
+
+INSERT INTO Profil (id_profil,firstname,lastname,profilImage,borndate,bornAt,maritalStatus,numberChild,drivingLicence,id_user,id_city,id_country)
+VALUE(11,'Tchoupo','anaurd','png','19/10/1992','bafoussam','couple','2','permis C',11,11,11);
+
+INSERT INTO Profil (id_profil,firstname,lastname,profilImage,borndate,bornAt,maritalStatus,numberChild,drivingLicence,id_user,id_city,id_country)
+VALUE(12,'dongmo','ariel','png','01/10/1955','kribi','Celibataire','6','permis B',12,12,12);
+
+INSERT INTO Profil (id_profil,firstname,lastname,profilImage,borndate,bornAt,maritalStatus,numberChild,drivingLicence,id_user,id_city,id_country)
+VALUE(13,'douanla','maurice','png','11/12/1975','dschang','couple','0','permis C',13,13,13);
+
+INSERT INTO Profil (id_profil,firstname,lastname,profilImage,borndate,bornAt,maritalStatus,numberChild,drivingLicence,id_user,id_city,id_country)
+VALUE(14,'dongfack','anicet','png','30/04/1985','yaounde','Celibataire','5','permis A',14,14,14);
+
+INSERT INTO Profil (id_profil,firstname,lastname,profilImage,borndate,bornAt,maritalStatus,numberChild,drivingLicence,id_user,id_city,id_country)
+VALUE(15,'Tsafack','loice','png','11/10/1970','douala','couple','8','permis C',15,15,15);
--- Objects/Tables/Table_profil.sql
+++ Objects/Tables/Table_profil.sql
@@ -1,14 +1,13 @@
  CREATE TABLE Profil (
  id_profil INT, 
  firstname VARCHAR(50),
- lastname VARCHAR(50), 
- profilname VARCHAR(50), 
+ lastname VARCHAR(50),  
  profilImage VARCHAR(30), 
  borndate DATE,
  bornAt VARCHAR(20) , 
- maritalStatus VARCHAR(10) , 
+ maritalStatus VARCHAR(10), 
  numberChild INT, 
- drivingLicence VARCHAR(20) , 
+ drivingLicence VARCHAR(20), 
  id_user INT, 
  id_city INT, 
  id_country INT  
