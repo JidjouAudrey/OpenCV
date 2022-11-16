ecbd76a124ee6bab5b23bf3a3dae20b7ed15d080
Jenny Mbah <jennycynthiambah@gmail.com>
2022-11-16 19:34:22 +0100

    ajout des sequence

--- Objects/Sequence.sql
+++ Objects/Sequence.sql
@@ -1,7 +1,11 @@
-DROP  SEQUENCE   seq_experience;
-DROP  SEQUENCE   seq_project;
-DROP  SEQUENCE   seq_phones;
-DROP  SEQUENCE   seq_language;
+DROP SEQUENCE seq_experience;
+DROP SEQUENCE seq_project;
+DROP SEQUENCE seq_phones;
+DROP SEQUENCE seq_language;
+DROP SEQUENCE seq_hobbies;
+DROP SEQUENCE seq_users;
+DROP SEQUENCE seq_socialnetwork;
+DROP SEQUENCE seq_professional_exp;
 
 
 ************Sequence_Experience**************
@@ -30,5 +34,33 @@ increment BY 1;
 
 
 CREATE SEQUENCE seq_language
+START WITH 16
+increment BY 1;
+
+
+**************Sequence Hobbies******************
+
+CREATE SEQUENCE seq_hobbies
+START WITH 16
+increment BY 1;
+
+
+**************Sequence Users******************
+
+CREATE SEQUENCE seq_hobbies
+START WITH 16
+increment BY 1;
+
+
+**************Sequence SocialNetwork*****************
+
+CREATE SEQUENCE seq_hobbies
+START WITH 16
+increment BY 1;
+
+
+**************Sequence Professional_Exp*****************
+
+CREATE SEQUENCE seq_professional_exp 
 START WITH 16
 increment BY 1;
